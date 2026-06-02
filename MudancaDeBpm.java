28fc3ce33cbc5db72b11b8f4e4cabaf0af62a7e3
felago <felipelagosilva@hotmail.com>
2026-06-02 19:15:46 -0300

    changeable bpm, new error log messages and changed API magic numbers for constants

---
+++ MudancaDeBpm.java
@@ -0,0 +1,29 @@
+package gerador.dominio;
+
+/**
+ * Evento de mudança de BPM. Duração 0 pois não avança o tempo musical,
+ * apenas altera o andamento a partir deste ponto.
+ */
+public class MudancaDeBpm extends EventoMusical {
+
+    private final int novoBpm;
+
+    public MudancaDeBpm(int novoBpm) {
+        super(0);
+        this.novoBpm = novoBpm;
+    }
+
+    public int getNovoBpm() {
+        return novoBpm;
+    }
+
+    @Override
+    public boolean produzSom() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("MudancaDeBpm[bpm=%d]", novoBpm);
+    }
+}
